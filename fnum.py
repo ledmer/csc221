84f0bb40f195483017c3d0436748e6154d1080bf
ledmer <ledmer13@gmail.com>
2023-07-14 11:35:39 -0400

    play 2

--- fnum.py
+++ fnum.py
@@ -1,9 +1,9 @@
+from getpass import getpass
 import random
 from gasp.utils import read_yesorno
 
 def find_rangn():
     try:
-        rang = 0
         rang = int(input("Give a number as max range: "))
         while rang <= 1:
             print("not numbers less than 1 :3")
@@ -32,12 +32,33 @@ def find_n(f):
         find_n(f)
     else:
         print(f"it took you {f} tries")
+    
+def player_ans():
+    try:
+        rang = int(input("Give a number as max range: "))
+        while rang <= 1:
+            print("not numbers less than 1 :3")
+            rang = int(input("Give a number as max range: "))
+        numb = int(getpass("Secret number: "))
+        while numb > rang or numb < 0:
+            print("Give a number inside the range!!")
+            numb = getpass("Secret number: ")
+        print("\n" + f"Guess a number between 0 and {rang}")
+        return numb
+    except ValueError:
+        print("Use numbers!!")
+        find_rangn()
 
 game = True
 while game == True:
+    player2 = read_yesorno('Wanna play 2 player game?')
     i = 1
-    rann = find_rangn()
-    find_n(i)
+    if player2 == False:
+        rann = find_rangn()
+        find_n(i)
+    elif player2 == True:
+        rann = player_ans()
+        find_n(i)
     game = read_yesorno('Would you like another game? ')
 
 
