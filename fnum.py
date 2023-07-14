20f5cec37e4fe71a57fcddaed70f7fbac5a61a47
ledmer <ledmer13@gmail.com>
2023-07-14 10:03:33 -0400

    not two players not computer guessing

--- fnum.py
+++ fnum.py
@@ -1,24 +1,43 @@
 import random
-i = 1
+from gasp.utils import read_yesorno
+
+def find_rangn():
+    try:
+        rang = 0
+        rang = int(input("Give a number as max range: "))
+        while rang <= 1:
+            print("not numbers less than 1 :3")
+            rang = int(input("Give a number as max range: "))
+        print(f"Guess a number between 0 and {rang}")
+        rann = random.randint(0, rang)
+        return rann
+    except ValueError:
+        print("Use numbers greater than 1!!")
+        find_rangn()
+
 def find_n(f):
     try:
-        ans = int(input(f"Guess{f}: "))
-        while not ans == x:
-            if ans > x:
+        ans = float(input(f"Guess{f}: "))
+        while not ans == rann:
+            if ans > rann:
                 print("Too High!")
                 f += 1
-                ans = int(input(f"Guess{f}: "))
-            elif ans < x:
+                ans = float(input(f"Guess{f}: "))
+            elif ans < rann:
                 print("Too Low!")
                 f += 1
-                ans = int(input(f"Guess{f}: "))
-    except:
-        print("use numbers as output")
+                ans = float(input(f"Guess{f}: "))
+    except ValueError:
+        print("use numbers as input")
         find_n(f)
-    print(f"it took you {f} tries")
+    else:
+        print(f"it took you {f} tries")
 
+game = True
+while game == True:
+    i = 1
+    rann = find_rangn()
+    find_n(i)
+    game = read_yesorno('Would you like another game? ')
 
-x = random.randint(0, 1000)
-print("Guess a number between 0 and 1000")
-find_n(i) 
 
