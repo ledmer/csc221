f5a81e9b6b17977954946ae6f34c1f08c11948e6
ledmer <ledmer13@gmail.com>
2023-07-17 08:48:02 -0400

    player changes

--- fnum.py
+++ fnum.py
@@ -15,50 +15,55 @@ def find_rangn():
         print("Use numbers greater than 1!!")
         find_rangn()
 
-def find_n(f):
+def find_n(f, games_played = 1):
     try:
-        ans = float(input(f"Guess{f}: "))
+        ans = float(input(f"Player{games_played} Guess{f}: "))
         while not ans == rann:
             if ans > rann:
                 print("Too High!")
                 f += 1
-                ans = float(input(f"Guess{f}: "))
             elif ans < rann:
                 print("Too Low!")
                 f += 1
-                ans = float(input(f"Guess{f}: "))
+            ans = float(input(f"Player{games_played} Guess{f}: "))
     except ValueError:
         print("use numbers as input")
         find_n(f)
     else:
         print(f"it took you {f} tries")
     
-def player_ans():
+def player_ans(games_played):
     try:
-        rang = int(input("Give a number as max range: "))
+        x = 1
+        if games_played % 2 == 0:
+            x += 1
+        rang = int(input(f"Player{x} Give a number as max range: "))
         while rang <= 1:
             print("not numbers less than 1 :3")
-            rang = int(input("Give a number as max range: "))
+            rang = int(input(f"Player{x} Give a number as max range: "))
         numb = int(getpass("Secret number: "))
         while numb > rang or numb < 0:
             print("Give a number inside the range!!")
-            numb = getpass("Secret number: ")
+            numb = getpass(f"Player{x} Secret number: ")
         print("\n" + f"Guess a number between 0 and {rang}")
         return numb
     except ValueError:
         print("Use numbers!!")
         find_rangn()
 
+games_played = 0
 game = True
 while game == True:
     player2 = read_yesorno('Wanna play 2 player game?')
     i = 1
+    print(f"Game {games_played}:")
     if player2 == False:
         rann = find_rangn()
         find_n(i)
     elif player2 == True:
-        rann = player_ans()
-        find_n(i)
+        rann = player_ans(games_played)
+        find_n(i, games_played % 2 + 1)
     game = read_yesorno('Would you like another game? ')
+    games_played += 1
 
 
