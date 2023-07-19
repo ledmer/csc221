d2268c6af80df974c2015043b7c4e32bd4894371
ledmer <ledmer13@gmail.com>
2023-07-19 10:14:52 -0400

    loose screen

--- t.py
+++ t.py
@@ -18,61 +18,72 @@ def start_screen():
     update_when('key_pressed')
     clear_screen()
 
+def GGame():
+    start_screen()
+    lines()
+    player_x = 5
+    player_y = 5
+    robot_x = 40
+    robot_y = 40
+    while True:
+        player = Circle((10 * player_x + 5, 10 * player_y + 5), 5, filled=True)
+        robot = Circle((10 * robot_x + 5, 10 * robot_y + 5), 5, filled=True, color="Red")
+        key = update_when('key_pressed')
+        remove_from_screen(player)
+        if key == 'KP_Right':
+            player_x += 1
+        elif key == 'KP_Left':
+            player_x -= 1
+        elif key == 'KP_Up':
+            player_y += 1
+        elif key == 'KP_Down':
+            player_y -= 1
+        elif key == 'KP_Prior':
+            player_x += 1 
+            player_y += 1
+        elif key == 'KP_Home':
+            player_x -= 1 
+            player_y += 1 
+        elif key == 'KP_End':
+            player_x -= 1
+            player_y -= 1 
+        elif key == 'KP_Next': 
+            player_x += 1 
+            player_y -= 1 
+        if player_x == 63:
+            player_x = 1
+        if player_y == 47:
+            player_y = 1
+        if player_x == 0:
+            player_x = 63
+        if player_y == 0:
+            player_y = 47
+        elif key == "q":
+            end_graphics()
+        if player_x == robot_x and player_y == robot_y: 
+            break
+
+        if player_x > robot_x:
+            robot_x += 1
+        elif player_x < robot_x:
+            robot_x -= 1
+        if player_y > robot_y:
+            robot_y += 1
+        elif player_y < robot_y:
+            robot_y -= 1
+        remove_from_screen(robot)
+    clear_screen()
+    Text("You Lost :'c",(320, 270), size=40)
+    key_text = Text("Press esc to stop playing", (320, 100), size=12)
+    man = update_when('key_pressed')
+    if man == "Escape":
+        end_graphics()
+    time.sleep(10)
+
+    GGame()
+
+
 begin_graphics(title = "Robots")
 
-start_screen()
-
-lines()
-player_x = 5
-player_y = 5
-robot_x = 40
-robot_y = 40
-while True:
-    player = Circle((10 * player_x + 5, 10 * player_y + 5), 5, filled=True)
-    robot = Circle((10 * robot_x + 5, 10 * robot_y + 5), 5, filled=True, color="Red")
-    key = update_when('key_pressed')
-    remove_from_screen(player)
-    if key == 'KP_Right':
-        player_x += 1
-    elif key == 'KP_Left':
-        player_x -= 1
-    elif key == 'KP_Up':
-        player_y += 1
-    elif key == 'KP_Down':
-        player_y -= 1
-    elif key == 'KP_Prior':
-        player_x += 1 
-        player_y += 1
-    elif key == 'KP_Home':
-        player_x -= 1 
-        player_y += 1 
-    elif key == 'KP_End':
-        player_x -= 1
-        player_y -= 1 
-    elif key == 'KP_Next': 
-        player_x += 1 
-        player_y -= 1 
-    if player_x == 63:
-        player_x = 1
-    if player_y == 47:
-        player_y = 1
-    if player_x == 0:
-        player_x = 63
-    if player_y == 0:
-        player_y = 47
-    elif key == "q": 
-        break
-
-
-    if player_x > robot_x:
-        robot_x += 1
-    elif player_x < robot_x:
-        robot_x -= 1
-    if player_y > robot_y:
-        robot_y += 1
-    elif player_y < robot_y:
-        robot_y -= 1
-    remove_from_screen(robot)
-    
-
+GGame()
 end_graphics()
