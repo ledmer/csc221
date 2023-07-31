c0f7fdd0b867cbb86a222b036f3475862b30d4d3
ledmer <ledmer13@gmail.com>
2023-07-31 10:19:30 -0400

    test

--- test.py
+++ test.py
@@ -1,107 +1,108 @@
- from gasp import * # As usual
- import random
- 
+from gasp import * # As usual
+import random
+
 
 class Player:
- pass
- 
+    pass
+
+
 class Robot:
-  pass
- 
- 
-  def place_player():
-  global player
- 
-  player = Player()
-  player.x = random.randint(0, 63)
-  player.y = random.randint(0, 47)
- 
-  def safely_place_player():
-  global player
- 
-  place_player()
- 
-  while collided(player, robots):
-  place_player()
-  player.shape = Circle(
-  (10 * player.x + 5, 10 * player.y + 5), 5, filled=True
-  )
- 
- 
-  def place_robots():
-  global robots
- 
-  robots = []
- 
-  while len(robots) < numbots:
-  robot = Robot()
- 
-  robot.x = random.randint(0, 63)
-  robot.y = random.randint(0, 47)
-  robot.junk = False
-  robot.shape = Box((10 * robot.x, 10 * robot.y), 10, 10)
-  robots.append(robot)
- 
- 
-robots.py[+] Page 2
-  def move_player():
-  global player
- 
-  key = update_when("key_pressed")
- 
-  while key == "h":
-  remove_from_screen(player.shape)
-  safely_place_player()
-  key = update_when("key_pressed")
- 
-  if key == "b":
-  if player.x > 0:
-  player.x −= 1
-  if player.y > 0:
-  player.y −= 1
-  elif key == "n" and player.y > 0:
-  player.y −= 1
-  elif key == "m":
-  if player.x < 63:
-  player.x += 1
-  if player.y > 0:
- player.y −= 1
-  elif key == "g" and player.x > 0:
-  player.x −= 1
-  elif key == "j" and player.x < 63:
-  player.x += 1
-  elif key == "t":
-  if player.x > 0:
-  player.x −= 1
-  if player.y < 47:
-  player.y += 1
-  elif key == "y" and player.y < 47:
-  player.y += 1
-  elif key == "u":
-  if player.x < 63:
-  player.x += 1
-  if player.y < 47:
-  player.y += 1
- 
-  move_to(player.shape, (10 * player.x + 5, 10 * player.y + 5))
- 
- 
+    pass
+
+
+def place_player():
+    global player
+
+    player = Player()
+
+    player.x = random.randint(0, 63)
+    player.y = random.randint(0, 47)
+
+
+def safely_place_player():
+    global player
+
+    place_player()
+
+    while collided(player, robots):
+        place_player()
+
+    player.shape = Circle(
+    (   10 * player.x + 5, 10 * player.y + 5), 5, filled=True
+    )
+
+
+def place_robots():
+    global robots
+
+    robots = []
+
+    while len(robots) < numbots:
+        robot = Robot()
+        robot.x = random.randint(0, 63)
+        robot.y = random.randint(0, 47)
+        robot.junk = False
+        robot.shape = Box((10 * robot.x, 10 * robot.y), 10, 10)
+        robots.append(robot)
+
+
+
+def move_player():
+    global player
+
+    key = update_when("key_pressed")
+
+    while key == "h":
+        remove_from_screen(player.shape)
+        safely_place_player()
+        key = update_when("key_pressed")
+
+    if key == "b":
+        if player.x > 0:
+            player.x -= 1
+        if player.y > 0:
+            player.y -= 1
+    elif key == "n" and player.y > 0:
+        player.y -= 1
+    elif key == "m":
+        if player.x < 63:
+            player.x += 1
+        if player.y > 0:
+            player.y -= 1
+    elif key == "g" and player.x > 0:
+        player.x -= 1
+    elif key == "j" and player.x < 63:
+        player.x += 1
+    elif key == "t":
+        if player.x > 0:
+            player.x -= 1
+        if player.y < 47:
+            player.y += 1
+    elif key == "y" and player.y < 47:
+        player.y += 1
+    elif key == "u":
+        if player.x < 63:
+            player.x += 1
+        if player.y < 47:
+            player.y += 1
+
+    move_to(player.shape, (10 * player.x + 5, 10 * player.y + 5))
 def move_robots():
   global robots
   for robot in robots:
     if not robot.junk:
         if robot.x > player.x:
-            robot.x −= 1
+            robot.x -= 1
         elif robot.x < player.x:
             robot.x += 1
         if robot.y > player.y:
-            robot.y −= 1
+            robot.y -= 1
         elif robot.y < player.y:
             robot.y += 1
         move_to(robot.shape, (10 * robot.x, 10 * robot.y))
  
  
-  def collided(thing1, list_of_things):
+def collided(thing1, list_of_things):
     for thing2 in list_of_things:
         if thing1.x == thing2.x and thing1.y == thing2.y:
             return True
@@ -129,10 +130,10 @@ def check_collisions():
  
   # Handle robots crashing into each other
   surviving_robots = []
- 
+  
   for robot in robots:
     if collided(robot, junk):
-    continue
+        continue
  
     zombie = robot_crashed(robot)
     
